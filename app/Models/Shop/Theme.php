e1c74deac3daa53ae0e8aef3c8f20e0091d7b9ae
vitalii <sokilvitaliy01@gmail.com>
2021-05-13 00:37:47 +0300

    1

--- app/Models/Shop/Theme.php
+++ app/Models/Shop/Theme.php
@@ -7,12 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Theme extends Model
 {
+    protected $fillable = [
+        'name',
+        'image_preview',
+        'type'
+    ];
     use HasFactory;
 
     const LIST_TYPE = 'list-type';
     const BLOCK_TYPE = 'block-type';
 
-    public function shop(){
-        return $this->hasOne(Shop::class);
+    public function shops(){
+        return $this->hasMany(Shop::class);
     }
 }
